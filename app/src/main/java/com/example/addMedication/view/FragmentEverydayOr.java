b7bad92644f9cf67318c1cc1aeff67a7d7ef4793
Aya Hassan <54456549+AyaHassanAbdelghany@users.noreply.github.com>
2022-03-10 17:07:39 +0200

    edit and display screen , medication reminder dialog , add med NavGrap

---
+++ app/src/main/java/com/example/addMedication/view/FragmentEverydayOr.java
@@ -0,0 +1,27 @@
+package com.example.addMedication.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.medicalreminder.R;
+
+public class FragmentEverydayOr  extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.everyday_or_question_screen, container, false);
+        return view;
+
+    }
+}
